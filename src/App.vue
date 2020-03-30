6739a5f58caebb32d073bb5b0f8031eccade4f89
timprice-code <62757231+timprice-code@users.noreply.github.com>
2020-03-30 09:34:41 -0400

    testing commit from sandbox
    testing commit from sandbox

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+    <HelloWorld msg="Hello Vue in CodeSandbox - test edit!" />
   </div>
 </template>
 
